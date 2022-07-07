0ac3399d7d931ffee0666b30636f9b4f335cc36c
Ali <42435299+gcodiac@users.noreply.github.com>
2022-07-07 13:06:47 +0100

    PSR-2: Coding Style Guide - PHP-FIG

--- src/App/Commands/Payroll.php
+++ src/App/Commands/Payroll.php
@@ -1,13 +1,12 @@
 <?php
 namespace Console\App\Commands;
-use Console\App\Helpers\SaveToCsv;
 
- 
+use Console\App\Helpers\SaveToCsv;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
- 
+
 class Payroll extends Command
 {
     // date_default_timezone_set('GMT'); // set local time zone to Eastern Standard Time
@@ -20,85 +19,76 @@ class Payroll extends Command
             ->setHelp('Demonstration of custom commands created by Symfony Console component.')
             ->addArgument('outputFile', InputArgument::REQUIRED, 'Pass the output file name.');
     }
- 
+
     protected function execute(InputInterface $input, OutputInterface $output)
-    {        
+    {
         $fileName = $input->getArgument('outputFile');
         $fileName = $fileName . '.csv';
-        $output->writeln('The CSV file is geenerated successfully: '. $fileName); 
-
+        $output->writeln('The CSV file is geenerated successfully: ' . $fileName);
 
         $dt = new \DateTime();
 
         for ($i = 0; $i < 12; $i++) {
-            
+
             $month = $dt->format('Y-m-d');
 
-            // Get last day of the the given month 
+            // Get last day of the the given month
             $last_date = date("Y-m-t", strtotime($month));
 
             // Check if last day of the month is weekend
-            if($this->isWeekend($last_date) == "saturday"){
-                    $last_date = date('Y-m-d', strtotime($last_date .' -1 day'));
+            if ($this->isWeekend($last_date) == "saturday") {
+                $last_date = date('Y-m-d', strtotime($last_date . ' -1 day'));
 
-            }elseif($this->isWeekend($last_date) == "sunday"){
-                    $last_date = date('Y-m-d', strtotime($last_date .' -2 day'));
+            } elseif ($this->isWeekend($last_date) == "sunday") {
+                $last_date = date('Y-m-d', strtotime($last_date . ' -2 day'));
 
-            }else {
-                    $last_date = $last_date;
+            } else {
+                $last_date = $last_date;
             }
 
-                // get the 10th day of this month
-                $tenth_date =  date('Y-m-d', strtotime('+9 days', strtotime('first day of this month', strtotime($month))));
+            // get the 10th day of this month
+            $tenth_date = date('Y-m-d', strtotime('+9 days', strtotime('first day of this month', strtotime($month))));
 
-                // Check if 10th day of the month is weekend
-            if($this->isWeekend($tenth_date) == "saturday"){
-                    $adjusted_tenth_date = date('Y-m-d', strtotime($tenth_date .' +2 day'));
+            // Check if 10th day of the month is weekend
+            if ($this->isWeekend($tenth_date) == "saturday") {
+                $adjusted_tenth_date = date('Y-m-d', strtotime($tenth_date . ' +2 day'));
 
-            }elseif($this->isWeekend($tenth_date) == "sunday"){
-                $adjusted_tenth_date = date('Y-m-d', strtotime($tenth_date .' +1 day'));
+            } elseif ($this->isWeekend($tenth_date) == "sunday") {
+                $adjusted_tenth_date = date('Y-m-d', strtotime($tenth_date . ' +1 day'));
 
-            }else {
-                    $adjusted_tenth_date = $tenth_date;
+            } else {
+                $adjusted_tenth_date = $tenth_date;
             }
-
 
             // stores the pay dates
             $year = date('Y', strtotime($last_date));
             $month = date('F', strtotime($last_date));
-            $monthYear = $month . '/' . $year; 
+            $monthYear = $month . '/' . $year;
 
-
-
-            $data[] = array(1 => $monthYear, 2=> $last_date, 3 => $adjusted_tenth_date);
+            $data[] = array(1 => $monthYear, 2 => $last_date, 3 => $adjusted_tenth_date);
             // $dataa[] = array("name"=> $adjusted_tenth_date);
 
-
             $dt->modify('+1 month');
-            
+
         }
 
-        
         // Write result to CSV fuke.
         SaveToCsv::save($data, $fileName);
 
-
         return Command::SUCCESS;
 
-
     }
-
 
     private function isWeekend($last_date)
     {
 
-        $weekday= date("l", strtotime($last_date) );
+        $weekday = date("l", strtotime($last_date));
         $normalized_weekday = strtolower($weekday);
-    
+
         if ($normalized_weekday == "saturday") {
-            return $normalized_weekday; 
+            return $normalized_weekday;
         } elseif ($normalized_weekday == "sunday") {
-            return $normalized_weekday; 
+            return $normalized_weekday;
         }
 
     }
--- src/App/Helpers/SaveToCsv.php
+++ src/App/Helpers/SaveToCsv.php
@@ -4,21 +4,20 @@ namespace Console\App\Helpers;
 class SaveToCsv
 {
 
-    public static function save($data, $fileName){
+    public static function save($data, $fileName)
+    {
 
-
-         $f = fopen($fileName, 'w');
-         // Check if we were able to open the file.
-         if ($f) {
-             fputcsv($f, ['Period', 'Basic Payment', 'Bonus Payment']);
-             foreach ($data as $pay_date) {
-                 fputcsv($f,$pay_date);
-             }
-             fclose($f);
-         } else {
-             throw new \RuntimeException("Something Went Wrong!."); // TODO: improve it
-         }
-
+        $f = fopen($fileName, 'w');
+        // Check if we were able to open the file.
+        if ($f) {
+            fputcsv($f, ['Period', 'Basic Payment', 'Bonus Payment']);
+            foreach ($data as $pay_date) {
+                fputcsv($f, $pay_date);
+            }
+            fclose($f);
+        } else {
+            throw new \RuntimeException("Something Went Wrong!."); // TODO: improve it
+        }
 
     }
 
